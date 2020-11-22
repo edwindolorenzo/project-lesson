2071639af2fadcb75d26d316dec32b502979ff8b
edwindolorenzo <edwindo_lorenzo@yahoo.com>
2020-11-22 14:32:50 +0700

    deleting items

--- renderer/items.js
+++ renderer/items.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const { networkInterfaces } = require('os')
 
 // Dom nodes
 let items = document.getElementById('items')
@@ -14,6 +15,46 @@ fs.readFile(`${__dirname}/reader.js`, (err, data) =>{
 // Track items in storage
 exports.storage = JSON.parse(localStorage.getItem('readit-items')) || []
 
+// Listen for "done" message from reader window
+window.addEventListener('message', e =>{
+  // console.log(e.data)
+
+  // Check for correct message
+  if (e.data.action === 'delete-reader-item'){
+    // Delete item at given index
+    this.delete(e.data.itemIndex)
+    
+    // Close the reader window
+    e.source.close()
+  }
+})
+
+// Delete item
+exports.delete = itemIndex => {
+  items.removeChild(items.childNodes[itemIndex])
+
+  // Remove from storage
+  this.storage.splice(itemIndex, 1)
+  this.save()
+
+  if (this.storage.length){
+    let = newSelectedItemIndex = (itemIndex === 0 ) ? 0 : itemIndex - 1
+
+    document.getElementsByClassName('read-item')[newSelectedItemIndex].classList.add('selected')
+  }
+}
+
+exports.getSelectedItem = () =>{
+  let currentItem = document.getElementsByClassName('read-item selected')[0]
+
+  let itemIndex = 0
+  let child = currentItem
+  while( (child = child.previousElementSibling) != null) itemIndex++
+
+  // Return selected item and index
+  return { node: currentItem, index: itemIndex}
+}
+
 // Presist storage
 exports.save = () => {
   localStorage.setItem('readit-items', JSON.stringify(this.storage))
@@ -22,20 +63,20 @@ exports.save = () => {
 // set items as selected
 exports.select = e => {
   //remove currently selected item
-  document.getElementsByClassName('read-item selected')[0].classList.remove('selected')
+  this.getSelectedItem().node.classList.remove('selected')
   // add to clicked item
   e.currentTarget.classList.add('selected')
 }
 
 // change selection with arrow
 exports.changeSelection = direction =>{
-  let currentItem = document.getElementsByClassName('read-item selected')[0]
+  let currentItem = this.getSelectedItem()
   if (direction === 'ArrowUp' && currentItem.previousElementSibling) {
-    currentItem.classList.remove('selected')
-    currentItem.previousElementSibling.classList.add('selected')
+    currentItem.node.classList.remove('selected')
+    currentItem.node.previousElementSibling.classList.add('selected')
   } else if (direction === 'ArrowDown' && currentItem.nextElementSibling) {
-    currentItem.classList.remove('selected')
-    currentItem.nextElementSibling.classList.add('selected')
+    currentItem.node.classList.remove('selected')
+    currentItem.node.nextElementSibling.classList.add('selected')
   }
 
 }
@@ -46,10 +87,10 @@ exports.open = () => {
   // only if we have items (in case of menu open)
   if( !this.storage.length) return
 
-  let selectedItem = document.getElementsByClassName('read-item selected')[0]
+  let selectedItem = this.getSelectedItem()
 
   // get URL
-  let contentURL = selectedItem.dataset.url
+  let contentURL = selectedItem.node.dataset.url
 
   // console.log('Opening item' , contentURL)
 
@@ -65,7 +106,7 @@ exports.open = () => {
   `)
 
   // Inject JavaScript
-  readerWin.eval(readerJS)
+  readerWin.eval(readerJS.replace('{{index}}', selectedItem.index))
 }
 
 exports.addItem = (item, isNew = false) => {
--- renderer/reader.js
+++ renderer/reader.js
@@ -1,1 +1,42 @@
-alert('Hello from JS')
+// Create button in remote content to mark item as "Done"
+
+let readitClose = document.createElement('div')
+readitClose.innerText = 'Done'
+
+// Style button
+readitClose.style.position = 'fixed'
+readitClose.style.bottom = '15px'
+readitClose.style.right = '15px'
+readitClose.style.padding = '5px 10px'
+readitClose.style.fontSize = '20px'
+readitClose.style.fontWeight = 'bold'
+readitClose.style.background = 'dodgerblue'
+readitClose.style.color = 'white'
+readitClose.style.borderRadius = '5px'
+readitClose.style.cursor = 'default'
+readitClose.style.boxShadow = '2px 2px 2px rgba(0,0,0,0.2)'
+readitClose.style.zIndex = '9999'
+
+// Attach clik handler
+readitClose.onclick = e => {
+  // alert('Done with item')
+
+  // Message parent (Opener) window
+  window.opener.postMessage({
+    action: 'delete-reader-item',
+    itemIndex: {{index}}
+  }, '*')
+}
+
+
+
+// Append button to body
+
+// error  because from DOM html ( non standart javascript) you can use ipc renderer
+// document.getElementsByTagName('body')[0].appendChild(readitClose)
+// ==============================================
+// you can use other method
+
+// document.getElementsByTagName('body')[0].innerHtml += readitClose.outerHTML
+
+document.getElementsByTagName('body')[0].append(readitClose)
